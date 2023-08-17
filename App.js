feaa6a16c232743ddecd90e12e842ee06e61d3e1
yogeshwar1108 <130997217+yogeshwar1108@users.noreply.github.com>
2023-08-17 23:49:46 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,29 +1,15 @@
-// import './App.css';
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-// import Tproducts from "./components/Tproducts";
-import Dashboard from "./components/Dashboard";
-import Cart from "./components/Cart";
-import RootLayou from "./components/RootLayou";
+import { Provider } from "react-redux";
+import "./App.css";
+
+import Navbar from "./components/Navbar";
+import store from "./components/Redux/store";
 
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<RootLayou/>}>
-        <Route index element={<Dashboard />}></Route>
-        <Route path="/cart" element={<Cart />}></Route>
-      </Route>
-    )
-  );
-
   return (
     <div className="App">
-      <RouterProvider router={router}/>
-      {/* <Tproducts /> */}
+      <Provider store={store}>
+        <Navbar />
+      </Provider>
     </div>
   );
 }
